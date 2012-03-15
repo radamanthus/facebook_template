d604082774c6a36d6f2f439576a28a02934eb591
Radamanthus Batnag <rad@batnag.org>
2012-03-15 18:44:21 +0800

    Initial commit

---
+++ app/models/user.rb
@@ -0,0 +1,22 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable,
+         :omniauthable
+
+  # Setup accessible (or protected) attributes for your model
+  attr_accessible :email, :password, :password_confirmation, :remember_me
+
+  def self.find_for_facebook_oauth(access_token, signed_in_resource=nil)
+    data = access_token.extra.raw_info
+    if user = User.where(:email => data.email).first
+      user
+    else # Create a user with a stub password.
+      u = User.create!(:email => data.email, :password => Devise.friendly_token[0,20])
+      # This is needed becase facebook_id is a protected attribute
+      u.facebook_id = data.id
+      u.save
+    end
+  end
+end
