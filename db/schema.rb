fc00c0bf161faa9a877bb5767e5160e3921d77a4
radamanthus <rad@batnag.org>
2012-09-17 22:42:20 +0800

    Updated to latest Rails

--- db/schema.rb
+++ db/schema.rb
@@ -25,8 +25,6 @@ ActiveRecord::Schema.define(:version => 20120313132547) do
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
     t.string   "facebook_id"
-    t.datetime "created_at",                             :null => false
-    t.datetime "updated_at",                             :null => false
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
